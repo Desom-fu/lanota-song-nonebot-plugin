bc80e07de19673479a7220f6ba23990f282afadc
Desom-fu <desom233@outlook.com>
2025-08-16 14:26:23 +0800

    加上私聊白名单

--- lanota-song-nonebot-plugin/config.py
+++ lanota-song-nonebot-plugin/config.py
@@ -1,14 +1,15 @@
 from pathlib import Path
 
-lanota_group = 114514 # 这里不用管，已然废弃，填什么都行
-allowed_groups = {1037559220,551374760,565752728,1006108282} # 白名单群组，只有这些群才会触发
+lanota_group = 114514  # 这里不用管，已然废弃，填什么都行
+allowed_groups = {1037559220, 551374760, 565752728, 1006108282}  # 白名单群组，只有这些群才会触发
+allowed_users = {"121096913","2946244126","3631828847","2976143542"}  # 添加允许的私聊用户QQ号
 save_dir = Path("Data") / "generate_image" 
-backup_path = Path() / "Data" / "UserList_Backup"
-user_path = Path() / "Data" / "UserList"
+backup_path = Path("Data") / "UserList_Backup"
+user_path = Path("Data") / "UserList"
 file_name = "UserData.json"
 full_path = user_path / file_name
 font_path = Path("Data") / "fonts.ttf"
-lanota_data_path = Path() / "Data" / "LanotaSongList"
+lanota_data_path = Path("Data") / "LanotaSongList"
 lanota_file_name = "song_list.json"
 lanota_alias_name = "song_alias.json"
 lanota_table_name = "song_table.json"
--- lanota-song-nonebot-plugin/whitelist.py
+++ lanota-song-nonebot-plugin/whitelist.py
@@ -1,8 +1,14 @@
 from nonebot.rule import Rule
-from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent
-from .config import allowed_groups
+from nonebot.adapters.onebot.v11 import Bot, Event, GroupMessageEvent, PrivateMessageEvent
+from .config import allowed_groups, allowed_users
 
-async def group_whitelist(bot: Bot, event: GroupMessageEvent) -> bool:
-    return event.group_id in allowed_groups
+async def check_whitelist(bot: Bot, event: Event) -> bool:
+    if isinstance(event, GroupMessageEvent):
+        # 群聊检查群白名单
+        return event.group_id in allowed_groups
+    elif isinstance(event, PrivateMessageEvent):
+        # 私聊检查用户白名单
+        return str(event.user_id) in allowed_users
+    return False
 
-whitelist_rule = Rule(group_whitelist)
+whitelist_rule = Rule(check_whitelist)
